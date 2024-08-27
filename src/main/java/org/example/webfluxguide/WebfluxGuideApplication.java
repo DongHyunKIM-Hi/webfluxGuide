08150aa92d26d7d8b352ae46d428f96f11dd1338
Viva <happybird00@naver.com>
2024-08-28 00:01:13 +0800

    Initial commit

---
+++ src/main/java/org/example/webfluxguide/WebfluxGuideApplication.java
@@ -0,0 +1,13 @@
+package org.example.webfluxguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webfluxguide/WebfluxGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webfluxguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
